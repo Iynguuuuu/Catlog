d6c1748883fb8c78083d83549b7d0ac5eef04c6e
Iynguuuuu <140171430+Iynguuuuu@users.noreply.github.com>
2024-07-06 16:09:26 +0530

    Designed UI with tailwind css

--- pages/breed/[id].tsx
+++ pages/breed/[id].tsx
@@ -20,25 +20,25 @@ const BreedDetails = () => {
   }, [id, dispatch]);
 
   if (status === 'loading') {
-    return <div>Loading...</div>
+    return <div className="flex text-4xl items-center justify-center h-screen">Loading... ⌛</div>
   }
 
   if (!breed) {
-    return <div>Breed not found</div>;
+    return <div className="flex text-4xl items-center justify-center h-screen">Breed not found 🚫</div>;
   }
 
   return (
-    <div>
-      <div>
-        <h1>{breed.name}</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+        <h1 className="text-4xl font-bold mb-4">{breed.name}</h1>
         <img
-        
+          className="w-[500px] h-[400px] object-cover mx-auto mb-4"
           src={`https://cdn2.thecatapi.com/images/${breed.reference_image_id}.jpg`}
           alt={breed.name}
         />
-        <p><span>Origin:</span> {breed.origin}</p>
-        <p><span>Life Span:</span> {breed.life_span} years</p>
-        <p><span>Description </span><br />{breed.description}</p>
+        <p className="text-lg mb-2"><span className='font-bold'>Origin:</span> {breed.origin}</p>
+        <p className="text-lg mb-2"><span className='font-bold'>Life Span:</span> {breed.life_span} years</p>
+        <p className="text-lg mb-2"><span className='font-bold'>Description </span><br />{breed.description}</p>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,16 +15,16 @@ const Home = () => {
   }, [status, dispatch]);
 
   return (
-    <div>
-      <div>
-        <h1 >Cat Catalog</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center w-full max-w-md">
+        <h1 className="text-3xl font-bold text-yellow-500 mb-4" >Cat Catalog 🐱</h1>
         <ul>
           {breeds.map((breed) => (
-            <li key={breed.id} >
-              <Link href={`/breed/${breed.id}`} >
+            <li key={breed.id} className="border border-yellow-500 rounded-lg mb-2 p-4 flex justify-between items-center" >
+              <Link href={`/breed/${breed.id}`} className="flex-grow text-left" >
                 {breed.name}
               </Link>
-              <Link href={`/breed/${breed.id}`} >
+              <Link href={`/breed/${breed.id}`} className="text-black ml-2" >
               ⮞
               </Link>
             </li>
